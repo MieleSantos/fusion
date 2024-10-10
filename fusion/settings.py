10c0f3d42b2d6f8db97b7f6ceb7c7689cdf3061a
mielesantos <mielesnts@gmail.com>
2024-10-10 16:06:46 -0300

    add templates

--- fusion/settings.py
+++ fusion/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'stdimage',
 ]
 
 MIDDLEWARE = [
@@ -127,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
